5f7bd1526fc947784d4d5463cf0c684c8183c9b2
Logan <logan.langdon@yahoo.com>
2025-05-01 13:05:34 -0700

    Moved io to separate file

---
+++ src/PanicWriter.zig
@@ -0,0 +1,29 @@
+file: std.fs.File,
+
+pub fn writeAll(self: PanicWriter, bytes: []const u8) error{}!void {
+    self.file.writeAll(bytes) catch |err| {
+        std.debug.panic("write failed: {s}", .{@errorName(err)});
+    };
+}
+pub fn writeBytesNTimes(self: PanicWriter, bytes: []const u8, n: usize) error{}!void {
+    var i: usize = 0;
+    while (i < n) : (i += 1) {
+        _ = try self.write(bytes);
+    }
+}
+
+pub fn print(self: PanicWriter, comptime format: []const u8, args: anytype) void {
+    std.fmt.format(self, format, args) catch |err| {
+        std.debug.panic("fmt failed: {s}", .{@errorName(err)});
+    };
+}
+
+// Required for `std.fmt.format` to work (implements `std.io.Writer`)
+pub const Error = error{};
+pub fn write(self: PanicWriter, bytes: []const u8) Error!usize {
+    try self.writeAll(bytes);
+    return bytes.len;
+}
+
+const std = @import("std");
+const PanicWriter = @This();
--- src/main.zig
+++ src/main.zig
@@ -1,44 +1,35 @@
 var should_exit = false;
 const audio_file_default = "assets/beep.wav";
 
-fn writePanic(fd: std.fs.File, bytes: []const u8) error{}!usize {
-    return fd.write(bytes) catch |err| {
-        std.debug.panic("write failed with error: {s}", .{@errorName(err)});
-    };
-}
-const PanicWriter = struct {
-    file: std.fs.File,
-
-    pub fn writeAll(self: PanicWriter, bytes: []const u8) error{}!void {
-        self.file.writeAll(bytes) catch |err| {
-            std.debug.panic("write failed: {s}", .{@errorName(err)});
-        };
-    }
-    pub fn writeBytesNTimes(self: PanicWriter, bytes: []const u8, n: usize) error{}!void {
-        var i: usize = 0;
-        while (i < n) : (i += 1) {
-            _ = try self.write(bytes);
-        }
-    }
-
-    pub fn print(self: PanicWriter, comptime format: []const u8, args: anytype) void {
-        std.fmt.format(self, format, args) catch |err| {
-            std.debug.panic("fmt failed: {s}", .{@errorName(err)});
-        };
-    }
-
-    // Required for `std.fmt.format` to work (implements `std.io.Writer`)
-    pub const Error = error{};
-    pub fn write(self: PanicWriter, bytes: []const u8) Error!usize {
-        try self.writeAll(bytes);
-        return bytes.len;
-    }
-};
-
 pub const IOHandle = struct {
     out: PanicWriter,
     err: PanicWriter,
 };
+
+const Command = enum {
+    @"--set",
+    @"--break",
+    @"--sound",
+    @"--help",
+    invalid,
+};
+
+fn printHelp(io: IOHandle) void {
+    const help_menu =
+        \\usage: tblocks --set [topic,duration]... --break [break_time] --sound [path_to_sound_file]\n
+        \\  --set: Initialize a schedule with a list of pairs of topic and durations (HH:MM:SS) in the order in which they should be prioritized 
+        \\  --break: Set a break time (HH:MM:SS) between each task
+        \\  --sound: Set an alternate sound via a path to play as a notification for the end of each task
+        \\  --help: Print this menu
+    ;
+    io.out.print(help_menu, .{});
+}
+
+fn handleSigint(sig: c_int) callconv(.C) void {
+    _ = sig;
+    should_exit = true;
+    std.process.cleanExit();
+}
 
 pub fn main() !void {
     const stdout = std.io.getStdOut();
@@ -140,33 +131,9 @@ pub fn main() !void {
     std.Thread.sleep(std.time.ns_per_s);
 }
 
-fn printHelp(io: IOHandle) void {
-    const help_menu =
-        \\usage: tblocks --set [topic,duration]... --break [break_time] --sound [path_to_sound_file]\n
-        \\  --set 
-        \\  --break
-        \\  --sound
-        \\  --help: Print this menu
-    ;
-    io.out.print(help_menu, .{});
-}
-
-fn handleSigint(sig: c_int) callconv(.C) void {
-    _ = sig;
-    should_exit = true;
-    std.process.cleanExit();
-}
-
-const Command = enum {
-    @"--set",
-    @"--break",
-    @"--sound",
-    @"--help",
-    invalid,
-};
-
 const std = @import("std");
 const utils = @import("utils.zig");
 const Schedule = @import("Schedule.zig");
 const TimeFormat = @import("TimeFormat.zig");
 const AudioPlayer = @import("AudioPlayer.zig");
+const PanicWriter = @import("PanicWriter.zig");
